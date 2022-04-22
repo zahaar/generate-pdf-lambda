00e6b5381e3d2843e7493ca9a8752c6194b90620
Zahar Turkov <zahar.turkov@nbt.ag>
2022-04-22 02:57:07 +0200

    chore Clean code noise

--- src/app.js
+++ src/app.js
@@ -2,21 +2,13 @@
  *  SPDX-License-Identifier: MIT-0
  */
 
-const AWS = require('aws-sdk')
 const chromium = require('chrome-aws-lambda')
 
-
-const pageURL = process.env.TARGET_URL
 const agent =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
 exports.handler = async (event, context) => {
-  // console.log('Event')
-	console.log("EVENT: \n" + JSON.stringify(event.body, null, 2))
-
-	s = JSON.stringify(event.body)
-
-  let result = null
+  console.info("EVENT\n" + JSON.stringify(event, null, 2))
   let browser = null
   let pdf = null
 
@@ -35,7 +27,7 @@ exports.handler = async (event, context) => {
     await page.setUserAgent(agent)
 
     console.log('Sending HTML content to browser:')
-    await page.setContent(event.bo)
+    await page.setContent(event.body)
     await page.setViewport({
       width: 1080,
       height: 1600,
@@ -53,7 +45,7 @@ exports.handler = async (event, context) => {
     })
     console.log('Done writing PDF.')
 
-		console.log('Shutting down Chrome...')
+    console.log('Shutting down Chrome...')
     await page.close()
     await browser.close()
   } catch (error) {
@@ -74,9 +66,7 @@ exports.handler = async (event, context) => {
     },
     isBase64Encoded: true,
     body: pdf.toString('base64'),
-	}
-	// console.log(response)
+  }
 
-	var a = "temo"
   return response
 }
